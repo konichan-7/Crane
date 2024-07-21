d3ad9a0b3ec3ce2331c88059b3bd88d9a62249dd
konichan-7 <577994345@qq.com>
2024-07-21 20:38:42 +0800

    add yolov8 detect

---
+++ tasks/yolo/yolov8.hpp
@@ -0,0 +1,48 @@
+#ifndef TASKS__YOLOV8_HPP
+#define TASKS__YOLOV8_HPP
+
+#include <opencv2/opencv.hpp>
+#include <openvino/openvino.hpp>
+#include <string>
+#include <vector>
+
+namespace tasks
+{
+  struct Detection
+  {
+    int class_id;
+    float confidence;
+    cv::Rect box;
+    cv::Point2f center;
+
+    Detection(int class_id, float confidence, const cv::Rect &box)
+        : class_id(class_id), confidence(confidence), box(box),
+          center(box.x + box.width / 2.0f, box.y + box.height / 2.0f)
+    {
+    }
+  };
+
+  void draw_detections(
+      cv::Mat &img, const std::vector<Detection> &detections,
+      const std::vector<std::string> &classes = {});
+
+  class YOLOV8
+  {
+  public:
+    YOLOV8(const std::string &model_path, int class_num, const std::string &device = "AUTO");
+
+    std::vector<Detection> infer(const cv::Mat &bgr_img);
+
+  private:
+    int class_num_;
+    float nms_threshold_ = 0.3;
+    float score_threshold_ = 0.7;
+    ov::Core core_;
+    ov::CompiledModel compiled_model_;
+
+    std::vector<Detection> parse(double scale, cv::Mat &output) const;
+  };
+
+} // namespace tasks
+
+#endif // tasks__YOLOV8_HPP
